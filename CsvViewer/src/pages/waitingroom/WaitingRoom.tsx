e2d3e375536edecd28749e6b87bd4432a859bb3e
Da Hwa <john.213steven@gmail.com>
2025-11-22 03:08:49 +0700

    adding download functions

--- CsvViewer/src/pages/waitingroom/WaitingRoom.tsx
+++ CsvViewer/src/pages/waitingroom/WaitingRoom.tsx
@@ -1,47 +1,59 @@
-import { useState } from "react"
-import "../../index.css"
-import "./Waitingroom.css"
+import { useState, useEffect } from "react";
+import "../../index.css";
+import "./Waitingroom.css";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { PageRoutes } from "../../Constants";
 
-
 function WaitingRoom() {
-    const [searchParams] = useSearchParams();
-    const fileId = searchParams.get("id");
-    const fileName = searchParams.get("filename");
-    const fileType = searchParams.get("filetype");
+  const [searchParams] = useSearchParams();
+  const fileId = searchParams.get("id");
+  const fileName = searchParams.get("filename");
+  const fileType = searchParams.get("filetype");
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const [readyDownload, setReadyDownload] = useState(false);
-    const [percentage, setPercentage] = useState(100);
-    
-    const handleDownload = () => {
-        const token = ""
-        const url = `${PageRoutes.downloadpage.path}${token}`
-        navigate(url)
-    }
+  const [readyDownload, setReadyDownload] = useState(false);
+  const [percentage, setPercentage] = useState(50);
+
+    useEffect(() => {
+        if (percentage >= 100) {
+            setReadyDownload(true);
+        }
+    }, [percentage]);
+
+
+  const handleDownload = () => {
+    const token = "";
+    const url = `${PageRoutes.downloadpage.path}${token}`;
+    navigate(url);
+  };
 
   return (
     <div className="emptypage-container">
-        <div className="emptypage-topbox">
-            <h1>Converting CSV to Json</h1>
+      <div className="emptypage-topbox">
+        <h1>Converting CSV to JSON</h1>
+      </div>
+
+      <div className="emptypage-bottombox">
+        <div className="progresstxt-box">
+          <span>
+            Please wait ... [ <span className="percentage">{percentage}</span> %]
+          </span>
         </div>
-        <div className="emptypage-bottombox">
-            <div className="progresstxt-box">
-                <span>Please wait ... [ <span className="percentage">{percentage}</span> %]</span>
-            </div>
-            <div className="progressbar-box">
-                <div className="progressbar"></div>
-            </div>
+        <div className="progressbar-box">
+            <div className="progressbar" style={{ width: `${percentage}%` }}></div>
         </div>
-        <button disabled={!readyDownload} onClick={handleDownload}
+      </div>
+
+        <button
+            disabled={!readyDownload}
+            onClick={handleDownload}
             className={`downloadBtn ${!readyDownload ? "disabled" : ""}`}
         >
             Download JSON
         </button>
     </div>
-  )
+  );
 }
 
-export default WaitingRoom
+export default WaitingRoom;
